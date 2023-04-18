b0db5fadad68846ad469de96fcb22e51cf8675df
SirSZ8 <127765389+SirSZ8@users.noreply.github.com>
2023-04-18 19:04:22 +0700

    kelar

--- App.js
+++ App.js
@@ -1,24 +1,79 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Alert,
+  FlatList,
+} from "react-native";
 import ComponentPresentation from "./src/widgets/ComponentPresentation";
+import DATA from "./DATA";
+import DATA_SERVER from "./DATA_SERVER";
+
+const ListItem = ({ item }) => {
+  return (
+    <TouchableOpacity
+      onPress={() => Alert.alert("Barang", item.namaBarang)}
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        backgroundColor: "salmon",
+        borderBottomWidth: 0.5,
+        borderBottomColor: "white",
+      }}
+    >
+      <View>
+        <Text>{item.namaBarang}</Text>
+        <Text>{item.kodeBarang}</Text>
+      </View>
+      <View>
+        <Text>{item.hargaJual}</Text>
+        <Text>{item.hargaBeli}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 export default function App() {
+  const [products, setProducts] = useState(DATA);
+  const [isRefresh, setRefresh] = useState(false);
+
+  const callAPI = () => {
+    setRefresh(true);
+    setTimeout(() => {
+      setProducts(DATA);
+      console.log("Refresh kepanggil");
+      setRefresh(false);
+    }, 1000);
+  };
+
+  const infiniteScroll = () => {
+    setRefresh(true);
+    setTimeout(() => {
+      setProducts((values) => [...values, ...DATA_SERVER]);
+      console.log("Refresh kebawah kepanggil");
+      setRefresh(false);
+    }, 3000);
+  };
+
   return (
     <>
-      <View style={styles.heading}>
-        <Text style={styles.text}>Good Night, Zeas</Text>
-      </View>
       <View style={styles.container}>
-        <View style={styles.item1}>
-          <Text>Lorem ipsum</Text>
-        </View>
-        <View style={styles.item2}>
-          <Text>Dolor sit amet</Text>
-        </View>
-        <View style={styles.item3}>
-          <Text>Dolor sit memet</Text>
-        </View>
+        <FlatList
+          onRefresh={callAPI}
+          onEndReached={infiniteScroll}
+          refreshing={isRefresh}
+          data={products}
+          style={{ flex: 1, backgroundColor: "blue" }}
+          renderItem={({ item }) => <ListItem item={item} />}
+          keyExtractor={(item, index) => index}
+        />
       </View>
     </>
   );
@@ -37,10 +92,11 @@ const styles = StyleSheet.create({
     flex: 1,
     // backgroundColor: "blue",
     // alignItems: "center",
-    flexDirection: "row",
+    // flexDirection: "row",
     justifyContent: "center",
+    marginVertical: 20,
     paddingVertical: 32,
-    gap: 20,
+    gap: 8,
   },
   item1: {
     height: "20%",
